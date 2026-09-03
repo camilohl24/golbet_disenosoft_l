3acf16b9e12cd386eb9b977661268e5198d32db9
camilohl24 <juancamilohernandezhl@gmail.com>
2026-09-02 20:55:19 -0500

    Add generic repository, match repository and database seeder (Module 3)

---
+++ GolBet.Repositories/Interfaces/IGenericRepository.cs
@@ -0,0 +1,29 @@
+﻿
+
+using GolBet.Entities.Common;
+
+
+
+namespace GolBet.Repositories.Interfaces;
+
+public interface IGenericRepository<T> where T : AuditableEntity
+
+{
+
+    // ---- Queries ---- 
+
+    Task<IEnumerable<T>> GetAllAsync(bool includeInactive = false);
+
+    Task<T?> GetByIdAsync(int id);
+
+
+
+    // ---- Commands ---- 
+
+    Task<T> AddAsync(T entity);
+
+    Task UpdateAsync(T entity);
+
+    Task DeactivateAsync(int id);   // logical delete: IsActive = false 
+
+}
