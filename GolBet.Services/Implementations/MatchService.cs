7ca240a73d12ccf55cfd20c8d13ecf428944a79c
camilohl24 <juancamilohernandezhl@gmail.com>
2026-09-18 22:17:02 -0500

    Add services layer, DTOs, AutoMapper and match board (Module 4)

---
+++ GolBet.Services/Implementations/MatchService.cs
@@ -0,0 +1,27 @@
+﻿
+// GolBet.Services/Implementations/MatchService.cs
+using GolBet.Entities.Enums;
+using GolBet.Repositories.Interfaces;
+using GolBet.Services.Interfaces;
+using GolBet.Services.DTOs;
+using AutoMapper;
+
+namespace GolBet.Services.Implementations;
+
+public class MatchService : IMatchService
+{
+    private readonly IMatchRepository _matchRepository;
+    private readonly IMapper _mapper;
+
+    public MatchService(IMatchRepository matchRepository, IMapper mapper)
+    {
+        _matchRepository = matchRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<IEnumerable<MatchDto>> GetBoardAsync(MatchStatus? status = null)
+    {
+        var matches = await _matchRepository.GetAllWithTeamsAsync(status);
+        return _mapper.Map<IEnumerable<MatchDto>>(matches);
+    }
+}
--- GolBet.Web/Program.cs
+++ GolBet.Web/Program.cs
@@ -1,6 +1,9 @@
 using GolBet.Repositories.Data;
 using GolBet.Repositories.Implementations;
 using GolBet.Repositories.Interfaces;
+using GolBet.Services.Implementations;
+using GolBet.Services.Interfaces;
+using GolBet.Services.Mapping;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,8 +17,9 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 // Open generic registration: one line, a repository for every entity 
 
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
-
-
+builder.Services.AddScoped<IMatchRepository, MatchRepository>();
+builder.Services.AddAutoMapper(typeof(MappingProfile));
+builder.Services.AddScoped<IMatchService, MatchService>();
 
 // Specific repositories 
 
